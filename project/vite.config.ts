60b6b4ab0095ccdff5b39f91713127e308df09dd
gpudewell <greg@digitalempathyinc.com>
2025-05-30 13:47:58 -0700

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/<REPO_NAME>/", // User will replace <REPO_NAME>
+  base: "/customer_portal/", // User will replace <REPO_NAME>
   build: {
     outDir: "dist",
   },
